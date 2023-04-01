f6e4016d98011f03e4202ab1bec50b713e88dcdb
monoLb <129582218+monoLb@users.noreply.github.com>
2023-04-01 15:50:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ const port = process.env.PORT || 80
 app.listen(port, () => {
   console.log('服务启动成功，端口：', port)
 })
+// 一个用户发什么消息，就反弹什么消息的消息回复功能
+router.post('/message/post', async ctx => {
+  const { ToUserName, FromUserName, Content, CreateTime } = ctx.request.body;
+
+  ctx.body = {
+    ToUserName: FromUserName,
+    FromUserName: ToUserName,
+    CreateTime: +new Date(),
+    MsgType: 'text',
+    Content: `反弹你发的消息：${Content}`,
+  };
+});
